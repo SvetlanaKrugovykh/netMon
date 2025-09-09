a71973d42ed676e72299c03b45ceea43dcd9d102
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-09-09 16:44:31 +0200

    added snmp check test

---
+++ tests/checksnmpObjectStatus.test.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const { checksnmpObjectStatus } = require('../src/server/services/snmpNetWatchService')
+
+const TEST_SNMP_IP = process.env.TEST_SNMP_IP
+const TEST_SNMP_OID = process.env.TEST_SNMP_OID
+const TEST_SNMP_VALUE = process.env.TEST_SNMP_VALUE || ''
+const TEST_SNMP_MIN = process.env.TEST_SNMP_MIN || ''
+const TEST_SNMP_MAX = process.env.TEST_SNMP_MAX || ''
+const TEST_SNMP_DESCRIPTION = process.env.TEST_SNMP_DESCRIPTION || ''
+
+if (!TEST_SNMP_IP || !TEST_SNMP_OID) {
+  console.error('Please set TEST_SNMP_IP and TEST_SNMP_OID in your .env file')
+  process.exit(1)
+}
+
+const testSnmpObject = {
+  ip_address: TEST_SNMP_IP,
+  oid: TEST_SNMP_OID,
+  value: TEST_SNMP_VALUE,
+  min: TEST_SNMP_MIN,
+  max: TEST_SNMP_MAX,
+  description: TEST_SNMP_DESCRIPTION,
+  status: 'alive', // or 'dead', depending on what you want to test
+}
+
+console.log('Testing checksnmpObjectStatus with:', testSnmpObject)
+
+checksnmpObjectStatus(testSnmpObject)
+  .then(() => console.log('checksnmpObjectStatus executed (check logs for result)'))
+  .catch(err => console.error('Error during checksnmpObjectStatus test:', err))
