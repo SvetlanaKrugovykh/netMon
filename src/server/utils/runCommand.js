5a6887a4a2069c3b8a99545a60e209a344b69c06
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-09-11 18:36:41 +0200

    console.error(`Command failed:

--- src/server/utils/runCommand.js
+++ src/server/utils/runCommand.js
@@ -2,30 +2,31 @@ const { spawn } = require('child_process')
 
 async function runCommand(command, args = [], options = {}) {
   return new Promise((resolve, reject) => {
-    const process = spawn(command, args, { 
+    const process = spawn(command, args, {
       stdio: ['pipe', 'pipe', 'pipe'],
-      ...options 
+      ...options
     })
-    
+
     let stdout = ''
     let stderr = ''
-    
+
     process.stdout.on('data', (data) => {
       stdout += data.toString()
     })
-    
+
     process.stderr.on('data', (data) => {
       stderr += data.toString()
     })
-    
+
     process.on('close', (code) => {
       if (code === 0) {
         resolve({ stdout, stderr, exitCode: code })
       } else {
-        reject(new Error(`Command failed with exit code ${code}: ${stderr}`))
+        console.error(`Command failed: ${command} ${args.join(' ')}\n${stderr.trim()}`)
+        reject(new Error(stderr.trim() || `Command failed with exit code ${code}`))
       }
     })
-    
+
     process.on('error', (error) => {
       reject(error)
     })
