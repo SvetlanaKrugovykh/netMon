6c6270edca7dcacf9fb9d7f9c0b1e589e9b35cb0
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-09-12 15:07:26 +0200

    +

--- tests/test_snmp_remote_runCommand.js
+++ tests/test_snmp_remote_runCommand.js
@@ -2,8 +2,8 @@ const { runCommand } = require('../src/server/utils/commandsOS')
 require('dotenv').config()
 
 async function test() {
-  const command = 'snmpwalk -v 2c -c public -OXsq -On 192.168.165.205 1.3.6.1.2.1.1.1.0'
-  const testIp = process.env.TEST_SNMP_IP || '192.168.165.202'
+  const command = 'snmpwalk'
+  const testIp = process.env.TEST_SNMP_IP
   const args = [
     '-v', '2c',
     '-c', 'public',
