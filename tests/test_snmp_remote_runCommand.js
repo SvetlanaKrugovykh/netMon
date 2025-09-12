2874a3fecb5bbc9bd087270e558b05119780b26c
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-09-12 17:18:24 +0200

    +

--- tests/test_snmp_remote_runCommand.js
+++ tests/test_snmp_remote_runCommand.js
@@ -2,15 +2,17 @@ const { runCommand } = require('../src/server/utils/commandsOS')
 require('dotenv').config()
 
 async function test() {
-  const command = 'snmpwalk'
   const testIp = process.env.TEST_SNMP_IP
-  const args = [
-    '-v', '2c',
-    '-c', 'public',
-    '-OXsq', '-On',
-    testIp,
-    '1.3.6.1.2.1.1.1.0'
-  ]
+  const command = 'snmpwalk -v 2c -c public -OXsq -On ' + testIp + ' 1.3.6.1.2.1.1.1.0'
+
+  const args = []
+  // [
+  //   '-v', '2c',
+  //   '-c', 'public',
+  //   '-OXsq', '-On',
+  //   testIp,
+  //   '1.3.6.1.2.1.1.1.0'
+  // ]
   const value = '???'
   console.log('Test 1: SNMPwalk with value')
   const result1 = await runCommand(command, args, value)
